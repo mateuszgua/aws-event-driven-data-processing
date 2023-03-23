670aeb4c8d3636306fc5b4012fbbff2221ca1232
Mateusz Gula <mateuszgua@wp.eu>
2023-03-23 20:04:32 +0100

    Feat: add function for upload file into s3 bucket

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
+import config
 from get_json_data import get_api_get_random_users
+from upload_file_s3 import upload_file
 
 number_of_users = 5000
 
-response = get_api_get_random_users(number_of_users)
+# response = get_api_get_random_users(number_of_users)
+# print(response)
+path_to_upload = './data/sample.json'
+bucket_name = config.BUCKET_NAME
+object_name = 'dummy_users_data.json'
+response = upload_file(path_to_upload, bucket_name, object_name)
 print(response)
