73bf77c9bc06be9de7150f4c7d4d757a756161fd
Mateusz Gula <mateuszgua@wp.eu>
2023-03-23 20:49:55 +0100

    Reactor: add directory for app

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import config
-from get_json_data import get_api_get_random_users
-from upload_file_s3 import upload_file
+import app.config as config
+from app.get_json_data import get_api_get_random_users
+from app.upload_file_s3 import upload_file
 
 number_of_users = 5000
 
